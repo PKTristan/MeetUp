dc817c18bb90813e1888748f5c09b6bc28a05ae6
PKTristan <tcalderon1922@gmail.com>
2023-02-14 19:43:37 -0800

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,23 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require('../../utils/auth.js');
 
 
 //GCOnnect restoreUser middleware to the API router
     //if current user session is valid, set req.user to the user inn the database
     //if current user session is not valid, set req.user to null
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({
+        requestBody: req.body
+    });
+});
 
 module.exports = router;
---
+++ backend/routes/api/session.js
@@ -0,0 +1,33 @@
+// backend/routes/api/session.js
+const express = require ('express');
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+const router = express.Router();
+
+// Login
+router.post('/', async (req, res, next) => {
+    //parse credential and password from body
+    const { credential, password } = req.body;
+
+    //call the login User method
+    const user = await User.login({ credential, password });
+
+    // send error if the login failed
+    if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+    }
+
+    //if the login didn't fail, set the token cookie
+    await setTokenCookie(res, user);
+
+    return res.json({
+        user
+    });
+});
+
+module.exports = router;
