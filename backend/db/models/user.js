90a4ef0751a617567f3a6c5c5ce24af9c632e75d
PKTristan <tcalderon1922@gmail.com>
2023-02-16 21:01:30 -0800

    added group migration, model, and demo seeder file

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -67,7 +67,9 @@ module.exports = (sequelize, DataTypes) => {
 
 
     static associate(models) {
-      // define association here
+      User.hasMany(models.Group, {
+        foreignKey: 'organizerId'
+      });
     }
   };
 
