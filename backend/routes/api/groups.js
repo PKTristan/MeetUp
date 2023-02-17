0328e0cdf7304b3d7c84a4ce39cd41a389579895
PKTristan <tcalderon1922@gmail.com>
2023-02-16 23:59:43 -0800

    added requireAuthorization

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,7 +1,7 @@
 //backend/routes/api/groups.js
 const express = require('express');
 
-const { requireAuth } = require('../../utils/auth');
+const { requireAuthentication } = require('../../utils/auth');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { Group } = require('../../db/models');
--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -1,7 +1,9 @@
+
 // backend/utils/auth.js
 const jwt = require('jsonwebtoken');
+
 const { jwtConfig } = require('../config');
-const { User } = require('../db/models');
+const { User, Group } = require('../db/models');
 
 const { secret, expiresIn } = jwtConfig;
 
@@ -54,7 +56,7 @@ const restoreUser = (req, res, next) => {
 
 
 // if not current user, then return error
-const requireAuth = async function (req, _res, next) {
+const requireAuthentication = async function (req, _res, next, ) {
     const { token } = req.cookies;
 
     //create err for unauthorized
@@ -69,7 +71,7 @@ const requireAuth = async function (req, _res, next) {
 
     try {
         const decodedToken = jwt.verify(token, secret);
-        const user = await User.findByPk(decodedToken.id);
+        const user = await User.scope('currentUser').findByPk(decodedToken.id);
 
         if (!user) {
             return next(err);
@@ -81,14 +83,62 @@ const requireAuth = async function (req, _res, next) {
         error.status = 401;
         return next(error);
     }
-
-    /* if (req.user) return next();
-
-    const err = new Error('Unauthorized');
-    err.title = 'Unauthorized';
-    err.errors = ['Unauthorized'];
-    err.status = 401;
-    return next(err); */
 };
 
-module.exports = { setTokenCookie, restoreUser, requireAuth };
+//permissions and roles
+const requireAuthorization = async function (reqRoles) {
+    return async (req, res, next) => {
+        const user = req.user; // get the authenticated user from the request
+        const hasRequiredRoles = true;
+
+        const { organizer, member, attendee} = reqRoles;
+
+        if(organizer) {
+            const isOrganizer = await Group.findByPk(groupId, {
+                where: {
+                    organizerId: user.id
+                }
+            });
+
+            if(!isOrganizer) {hasRequiredRoles = false};
+        }
+
+        if (member) {
+            const status = await Membership.findOne({
+                where: {
+                    userId: user.id,
+                    groupId: member.groupId
+                },
+                attributes: ['status']
+            });
+
+            if(!status || (status.status !== member.status)) {
+                hasRequiredRoles = false;
+            }
+        }
+
+        if(attendee) {
+            const status = await Attendence.findOne({
+                where: {
+                    userId: user.id,
+                    eventId: attendee.eventId
+                },
+                attributes: ['status']
+            });
+
+            if (!status || (status.status !== attendee.status)) {
+                hasRequiredRoles = false;
+            }
+        }
+
+        // If the user has the required roles and permissions, allow them to access the endpoint
+        if (hasRequiredRoles) {
+            return next();
+        }
+
+        // If the user does not have the required roles and permissions, respond with a 403 error
+        res.status(403).json({ error: 'Unauthorized' });
+    }
+};
+
+module.exports = { setTokenCookie, restoreUser, requireAuthentication, requireAuthorization };
