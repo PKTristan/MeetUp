dbe34b33d0b21c10262283d515981aeca40fd57c
PKTristan <tcalderon1922@gmail.com>
2023-02-17 21:41:52 -0800

    added the migration and model files for the database

---
+++ backend/db/models/eventimage.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class EventImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  EventImage.init({
+    eventId: DataTypes.INTEGER,
+    preview: DataTypes.BOOLEAN,
+    url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'EventImage',
+  });
+  return EventImage;
+};
