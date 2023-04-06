9eabda1e26899dc083c3889b7de843956af0a492
PKTristan <tcalderon1922@gmail.com>
2023-04-05 18:34:49 -0700

    dropdown menu complete

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,12 +1,12 @@
 // /frontend/src/components/Navigation/index.js
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import * as sessionActions from "../../store/session";
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
+import './Navigation.css';
 
 const Navigation = () => {
     const user = useSelector(sessionActions.userSelector);
-    const dispatch = useDispatch();
 
     //return a UL that acts as the navigation bar
     if (user) {
