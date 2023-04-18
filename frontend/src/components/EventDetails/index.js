980d66ccd47d48d89d893e4d218ba249dbd8040c
PKTristan <tcalderon1922@gmail.com>
2023-04-17 18:36:43 -0700

    finished styling

--- frontend/src/components/EventDetails/index.js
+++ frontend/src/components/EventDetails/index.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { currentUserSelector } from "../../store/session";
 import InterimModal from "../Modal";
 import Delete from "../Delete";
+import './EventDetails.css';
 
 
 const EventDetails = () => {
@@ -39,7 +40,7 @@ const EventDetails = () => {
     }, [dispatch, event]);
 
 
-    useEffect (() => {
+    useEffect(() => {
         if (event) {
             const dateArrStart = event.startDate.split(/T|\./);
             const dateArrEnd = event.endDate.split(/T|\./);
@@ -59,29 +60,33 @@ const EventDetails = () => {
             {
                 (group && event) ?
                     (<><div className="events-section-1">
-                        <p className="breadCrumb">{"< "} <NavLink to="/events">Events</NavLink></p>
+                        <p className="breadCrumb">{"< "} <NavLink to="/events" style={{color: "teal"}}>Events</NavLink></p>
                         <h1>{event.name}</h1>
-                        <p>Hosted by {group.Organizer.firstName} {group.Organizer.lastName}</p>
+                        <p className="hostName">Hosted by {group.Organizer.firstName} {group.Organizer.lastName}</p>
                     </div>
 
                         <div className="events-section-2">
                             <img src={event.previewImage} alt={event.name} />
                             <div className="event-info-box">
                                 <div className="group-info" >
-                                    <img src={group.previewImage} alt={group.name}></img>
-                                    <h4>{group.name}</h4>
-                                    <p>{group.city}, {group.state}</p>
+                                    <img src={(group.GroupImages.length > 0) ? group.GroupImages[0].url : ""} alt={group.name}></img>
+                                    <div>
+                                        <h4>{group.name}</h4>
+                                        <p>{group.city}, {group.state}</p>
+                                    </div>
                                 </div>
 
                                 <div className="start-end">
-                                    <i className="fa-regular fa-face-smile" />
-                                    <p>START <label  className="date-time">{dateStart} &#183; {timeStart}</label></p>
-                                    <p>END <label className="date-time">{dateEnd} &#183; {timeEnd}</label></p>
+                                    <div><i className="fa-regular fa-face-smile" /></div>
+                                    <div>
+                                        <p>START <label className="date-time">{dateStart} &#183; {timeStart}</label></p>
+                                        <p>END <label className="date-time">{dateEnd} &#183; {timeEnd}</label></p>
+                                    </div>
                                 </div>
 
                                 <div className="price">
                                     <i className="fa-regular fa-face-smile" />
-                                    <p>{(event.price > 0) ? Number(event.price).toFixed(2) : "Free"}</p>
+                                    <p>{(event.price > 0) ?'$' + Number(event.price).toFixed(2) : "Free"}</p>
                                 </div>
 
                                 <div className="type">
